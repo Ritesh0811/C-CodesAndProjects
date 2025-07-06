535805e7bec737290be9bf44fc13aa9885f3fa84
Ritesh Kapse <42062283+Ritesh0811@users.noreply.github.com>
2025-07-06 19:38:20 +0530

    Add files via upload

---
+++ 14 Heaps/heapclass1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+int main(){
+
+    // cout << "This is C++ Code " <<endl;
+    // int num;
+    // cout << "Give me any value" <<endl;
+    // cin >> num;
+
+    // cout << "Entered value was - " << num << endl;
+
+
+    return 0;
+}
